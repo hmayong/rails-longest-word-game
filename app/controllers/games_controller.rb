c7e1381c02ad089f7dc05d4b80c317268a5def09
mayong <hhmayong@gmail.com>
2019-08-19 18:43:52 +0800

    -msessions incomplete

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,38 @@
+require 'json'
+require 'open-uri'
+
+class GamesController < ApplicationController
+  attr_reader :letters
+  def new
+    @letters = Array.new(10) { ('A'..'Z').to_a.sample }
+  end
+
+  def score
+    @count = session[:score] = 0
+    @word = params[:word]
+    @grid = params[:grid]
+    if included?(@word.upcase, @grid)
+      if english_word?(@word)
+        @result = "Congratulations #{@word} is a valid English word."
+        @count += @word.length
+        raise
+      else
+        @result = "Sorry but #{@word} is not a valid english word"
+      end
+    else
+      @result = "Sorry but #{@word} cannot be built out of #{@grid}"
+    end
+  end
+
+  private
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    return json['found']
+  end
+
+  def included?(guess, grid)
+  guess.chars.all? { |letter| guess.count(letter) <= grid.count(letter) }
+  end
+end
